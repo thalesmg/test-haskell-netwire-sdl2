a820c864a6da5b0af452bb2f429d87ab8384631e
Thales Macedo Garitezi <thalesmg@gmail.com>
2019-09-05 07:29:58 -0300

    Colocando movimentos 2D

--- src/c3/Main.hs
+++ src/c3/Main.hs
@@ -1,41 +1,63 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Arrows #-}
+{-# LANGUAGE MultiWayIf #-}
 
 module Main where
 
 import Prelude hiding ((.), id)
 
 import qualified SDL as SDL
-import qualified Control.Monad as CM (when)
+import qualified Control.Monad as CM (when, unless)
 import Control.Monad.Fix (MonadFix)
 import Control.Wire
 import FRP.Netwire
 import qualified Data.Set as S
+import GHC.Stack
 
-acceleration :: (HasTime t s, Monad m, Monoid e) => Wire s e m (S.Set SDL.Keycode) Double
-acceleration =  pure 50 . when (S.member SDL.KeycodeRight)
-            <|> pure (-50) . when (S.member SDL.KeycodeLeft)
-            <|> pure 0
+type Vec = (Double, Double)
 
-velocity :: HasTime t s => Wire s e m (Double, Bool) Double
-velocity = integralWith (\collided a -> if collided then (- a) else a) 0
+baseAcc = 400
 
-position :: (HasTime t s, Monad m, Monoid e) => Wire s e m Double (Double, Bool)
-position = go 0
+acceleration :: (HasTime t s, Monad m, Monoid e) => Wire s e m (S.Set SDL.Keycode) Vec
+acceleration = proc keysDown -> do
+  let xSign = if | S.member SDL.KeycodeRight keysDown -> 1
+                 | S.member SDL.KeycodeLeft keysDown -> - 1
+                 | otherwise -> 0
+  let ySign = if | S.member SDL.KeycodeUp keysDown -> - 1
+                 | S.member SDL.KeycodeDown keysDown -> 1
+                 | otherwise -> 0
+  returnA -< (xSign * baseAcc, ySign * baseAcc)
+
+reflectV :: HasTime t s => Wire s e m (Double, Bool) Double
+reflectV = integralWith (\collided a -> if collided then (- a) else a) 0
+
+velocity :: (HasTime t s, MonadFix m) => Wire s e m (Vec, (Bool, Bool)) Vec
+velocity = proc ((aX, aY), (collidedX, collidedY)) -> do
+  aX' <- reflectV -< (aX, collidedX)
+  aY' <- reflectV -< (aY, collidedY)
+  returnA -< (aX', aY')
+
+position :: (HasTime t s, Monad m, Monoid e) => Wire s e m Vec (Vec, (Bool, Bool))
+position = go (0, 0)
   where
-    go x =
-      mkPure $ \ds v ->
+    go (x, y) =
+      mkPure $ \ds (vX, vY) ->
         let dt = realToFrac (dtime ds)
-            x' = x + v * dt
-            coll = x < 0 || x > 800 - 50
-            x'' = if coll then max 1 (min (799 - 50) x') else x'
-        in (Right (x'', coll), go x'')
+            x' = x + vX * dt
+            y' = y + vY * dt
+            collX = x < 0 || x > 800 - 50
+            collY = y < 0 || y > 600 - 50
+            x'' = if collX then max 1 (min (799 - 50) x') else x'
+            y'' = if collY then max 1 (min (599 - 50) y') else y'
+        in (Right ((x'', y''), (collX, collY)), go (x'', y''))
 
-challenge3 :: (HasTime t s, MonadFix m) => Wire s () m (S.Set SDL.Keycode) Double
+challenge3 :: (HasTime t s, MonadFix m) => Wire s () m (S.Set SDL.Keycode) Vec
 challenge3 = proc keysDown -> do
-  accel <- acceleration -< keysDown
-  rec (pos, coll) <- position -< vel
-      vel <- velocity -< (accel, coll)
+  (accelX, accelY) <- acceleration -< keysDown
+  rec (pos, (collX, collY)) <- position -< (velX, velY)
+      -- vel <- velocity -< (accel, coll)
+      velX <- reflectV -< (accelX, collX)
+      velY <- reflectV -< (accelY, collY)
   returnA -< pos
 
 handleKey event ks =
@@ -53,18 +75,21 @@ appLoop keysDown session wire renderer = do
   events <- SDL.pollEvents
   let qPressed = S.member SDL.KeycodeQ keysDown'
       keysDown' = foldr handleKey keysDown events
-  (x, wire') <- stepWire wire ds (Right keysDown')
-  let xpos = either (const 0) id x
+  (pos, wire') <- stepWire wire ds (Right keysDown')
+  let (xpos, ypos) = either (const (0, 0)) id pos
+  -- let (xpos, ypos) = (50, 50)
   SDL.rendererDrawColor renderer SDL.$= SDL.V4 0 0 255 255
   SDL.clear renderer
   SDL.rendererDrawColor renderer SDL.$= SDL.V4 0 0 0 255
-  SDL.fillRect renderer (Just $ SDL.Rectangle (SDL.P (SDL.V2 (truncate (xpos :: Double)) 50)) (SDL.V2 50 50))
+  SDL.fillRect renderer (Just $ SDL.Rectangle (SDL.P (SDL.V2 (truncate (xpos :: Double)) (truncate (ypos :: Double)))) (SDL.V2 50 50))
   SDL.present renderer
-  CM.when (not qPressed) (appLoop keysDown' session' wire' renderer)
+  CM.unless qPressed (appLoop keysDown' session' wire' renderer)
+  -- CM.unless qPressed (appLoop keysDown' session' wire renderer)
 
-main :: IO ()
+main :: HasCallStack => IO ()
 main = do
   SDL.initializeAll
   window <- SDL.createWindow "Tufe" SDL.defaultWindow
   renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
   appLoop S.empty clockSession_ challenge3 renderer
+  -- appLoop S.empty clockSession_ undefined renderer
